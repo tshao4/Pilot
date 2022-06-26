41c3e83e542d9b907d1367e469960d31f060a19a
Tianyu Shao <shtyboy@hotmail.com>
2022-06-26 14:47:17 +0800

    finish animation

--- engine/source/runtime/function/animation/animation_FSM.cpp
+++ engine/source/runtime/function/animation/animation_FSM.cpp
@@ -34,33 +34,85 @@ namespace Pilot
         {
             case States::_idle:
                 /**** [0] ****/
+                if (is_jumping)
+                {
+                    m_state = States::_jump_start_from_idle;
+                }
+                else if (is_moving)
+                {
+                    m_state = States::_walk_start;
+                }
                 break;
             case States::_walk_start:
                 /**** [1] ****/
+                if (is_clip_finish)
+                {
+                    m_state = States::_walk_run;
+                }
                 break;
             case States::_walk_run:
                 /**** [2] ****/
+                if (is_jumping)
+                {
+                    m_state = States::_jump_start_from_walk_run;
+                }
+                else if (start_walk_end && is_clip_finish)
+                {
+                    m_state = States::_walk_stop;
+                }
+                else if (!is_moving)
+                {
+                    m_state = States::_idle;
+                }
                 break;
             case States::_walk_stop:
                 /**** [3] ****/
+                if (!is_moving && is_clip_finish)
+                {
+                    m_state = States::_idle;
+                }
                 break;
             case States::_jump_start_from_idle:
                 /**** [4] ****/
+                if (is_clip_finish)
+                {
+                    m_state = States::_jump_loop_from_idle;
+                }
                 break;
             case States::_jump_loop_from_idle:
                 /**** [5] ****/
+                if (!is_jumping)
+                {
+                    m_state = States::_jump_end_from_idle;
+                }
                 break;
             case States::_jump_end_from_idle:
                 /**** [6] ****/
+                if (is_clip_finish)
+                {
+                    m_state = States::_idle;
+                }
                 break;
             case States::_jump_start_from_walk_run:
                 /**** [7] ****/
+                if (is_clip_finish)
+                {
+                    m_state = States::_jump_loop_from_walk_run;
+                }
                 break;
             case States::_jump_loop_from_walk_run:
                 /**** [8] ****/
+                if (!is_jumping)
+                {
+                    m_state = States::_jump_end_from_walk_run;
+                }
                 break;
             case States::_jump_end_from_walk_run:
                 /**** [9] ****/
+                if (is_clip_finish)
+                {
+                    m_state = States::_walk_run;
+                }
                 break;
             default:
                 break;
